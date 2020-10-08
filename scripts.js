a843c9fc834520c597f98d5017208f4cd712f99d
Vignesh-Prasad <48744150+Vignesh-Prasad@users.noreply.github.com>
2020-10-09 00:27:04 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+(function()
+{
+    "use strict";
+    angular.module('cssStyleApp',[])
+    .controller('cssStyleController',function($scope)
+    {
+        $scope.textcolor = "blue";
+    });
+})();
